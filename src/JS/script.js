cc545e42930e63ab23c7790f6858ffaa7eeca3f2
Edson Kelvin <edsonkelvin19@gmail.com>
2024-04-25 01:59:51 -0300

    Adicionado um botão no menu de navegação no cabeçalho melhorando a responsividade. Adicionado duas novas seções,"hard Skills e formação acadêmica.

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,6 +1,18 @@
+const btnMenu = document.querySelector('.btn-menu');
+
 const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
 
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('touchstart', toggleMenu);
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart')
+        event.preventDefault();
+    const navegacao = document.querySelector('.menu');
+    navegacao.classList.toggle('active');
+}
 
 botaoMostrarProjetos.addEventListener('click', () => {
     mostrarMaisProjetos();
